dbb65e6ae5b7e69f0c0b237c8bd6fc7d1b96670d
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2020-09-22 22:23:01 +0100

    Added pseudo code for the algorithm

--- day10.rs
+++ day10.rs
@@ -1,7 +1,7 @@
-//Populated the array
+//Algorithm basis is ready
 fn main() {
     const SIZE: usize = 5;
-    let asteroids = "
+    let asteroids_pos = "
 .#..#
 .....
 #####
@@ -12,21 +12,40 @@ fn main() {
     const SHARP: &str = "#";
     const POINT: &str = ".";
     let mut offset = 0;
-    let mut array: [[&str; SIZE]; SIZE] = [[""; SIZE]; SIZE];
-
-    for (index, c) in asteroids.chars().enumerate() {
+    let mut asteroids_arr: [[&str; SIZE]; SIZE] = [[""; SIZE]; SIZE];
+    let mut asteroids: Vec<(usize, usize)> = Vec::new(); 
+    
+    for (index, c) in asteroids_pos.chars().enumerate() {
         let c = &(c.to_string()[..]);
         match c {
             SHARP => {
-                array[(index-offset)/5 as usize][index-offset - ((index-offset)/5 as usize)*5] = SHARP;
+                asteroids_arr[(index-offset)/5 as usize][index-offset - ((index-offset)/5 as usize)*5] = SHARP;
+                asteroids.push(   (((index-offset)/5 as usize) ,  (((index-offset)/5 as usize)*5)) )
             },
             POINT => {
-                array[(index-offset)/5 as usize][index-offset - ((index-offset)/5 as usize)*5] = POINT;
+                asteroids_arr[(index-offset)/5 as usize][index-offset - ((index-offset)/5 as usize)*5] = POINT;
             },
             _ => {
                 offset += 1;    
             }
         }
     }
-    println!("Here is the array: {:?}", array)
+    //println!("Here is the array: {:?}", asteroids_arr)
+    
+    
+    //Algorithm 
+    
+    //Check all diagonals
+    //Add one and one  (bottom right)
+    //Add one and remove one (bottom left)
+    //Add one and remove one (top right)
+    //Remove one and add one (top left)
+    
+    /*for i in asteroids_arr.iter() {
+        println!("asteroids are: {:?}", i);
+    }*/
+    
+    for asteroid in asteroids {
+        println!("asteroids location {:?}", asteroid)
+    }
 }
