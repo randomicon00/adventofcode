b1de411bf742c2bd3121abedcc144d82f7d0d429
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2020-10-03 23:04:42 +0100

    Setting up tests

--- day2.rs
+++ day2.rs
@@ -1,4 +1,14 @@
 
 fn main() {
-  println!("Day 2 of the advent of code.")
+  println!("Day 2 of the advent of code.");
 }
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test() {
+        assert_eq!(true, true)
+    }
+}
+
