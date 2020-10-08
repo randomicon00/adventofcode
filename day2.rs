4f878be89cb5c1a408372a131bfcbbd67888fc13
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2020-10-08 23:07:55 +0100

    Solution to day2 advent of code problem

--- day2.rs
+++ day2.rs
@@ -1,8 +1,51 @@
-//TODO Tomorrow.
 fn main() {
-  println!("Day 2 of the advent of code.");
-}
+   
+    let initial_state = "1,9,10,3,2,3,11,0,99,30,40,50";
+   
+    let mut curr_state: Vec<u16> = initial_state
+                            .split(",")
+                            .map(|v| v.parse::<u16>().expect("Not a valid positive number"))
+                            .collect();
+   
 
+    for i in 0..(curr_state.len()/4) {
+       
+        if i*(4+1) >= curr_state.len() {
+            break;
+        }
+       
+        let idx = i*4;
+        println!("idx: {} {}", idx, i);
+        match curr_state[i*4] {
+            1 => {
+                let idx1 = (idx + 1) as usize;
+                let idx2 = (idx + 2) as usize;
+                let idx3 = (idx + 3) as usize;
+                let res = curr_state[curr_state[idx1] as usize] + curr_state[curr_state[idx2] as usize];
+                let res_idx = curr_state[idx3] as usize;
+                let result = &mut curr_state[res_idx];
+                *result = res;
+            },
+            2 => {
+                let idx1 = (idx + 1) as usize;
+                let idx2 = (idx + 2) as usize;
+                let idx3 = (idx + 3) as usize;
+               
+                let res = curr_state[curr_state[idx1] as usize] * curr_state[curr_state[idx2] as usize];
+                let res_idx = curr_state[idx3] as usize;
+                let result = &mut curr_state[res_idx];
+                *result = res;
+            },
+            99 => {
+                println!("Found a 99");
+                return;
+            },
+            _ => panic!("Wrong number."),
+           
+        }
+    }
+    println!("{:?}", curr_state);
+}
 
 #[cfg(test)]
 mod tests {
