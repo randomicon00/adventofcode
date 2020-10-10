808673f66ecd5e7e66956aec54de8582f2513e80
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2020-10-10 23:23:05 +0100

    Added number validation method

--- day4.rs
+++ day4.rs
@@ -9,16 +9,21 @@ they only ever increase or stay the same (like 111123 or 135679).
 // Puzzle input is 125730-579381
 
 fn valid_number(num: u32) -> bool {
+    let splitted = format!("{}", num);
+    
+    let initial = 0;
+    let digits: Vec<u32> = splitted.chars()
+                    .map(|x| x.to_string().parse::<u32>().unwrap())
+                    .collect();
+    println!("{:?}", digits);
     true
 }
 
 fn main() {
-    let puzzle_input: Vec<u32> = "125730-579381".split("-").map(|x| x.parse::<u32>().unwrap()).collect();
-    let splitted = format!("{}", &puzzle_input[0]);
-    let mut nums = splitted.chars();
-    println!("{:?}", puzzle_input);
-    println!("{:?}", nums.next().unwrap());
-    println!("{:?}", nums.next().unwrap());
-    println!("{:?}", nums.next().unwrap());
-    println!("{:?}", nums.next().unwrap());
+    let puzzle_input: Vec<u32> = "125730-579381"
+            .split("-")
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+            
+    valid_number(puzzle_input[0]);
 }
