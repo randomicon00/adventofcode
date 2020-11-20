92dddd9067ccdb4fb2e352f27585036da99f1562
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2020-11-20 21:52:22 +0100

    Add problem statement

--- day23.rs
+++ day23.rs
@@ -1,3 +1,8 @@
+/*
+Boot up all 50 computers and attach them to your network. 
+What is the Y value of the first packet sent to address 255?
+*/
+
 fn main() {
   println!("Day 23 advent of code solution.");
 }
