2df01d92ab8d637ecdadaa85223ef130279e17b3
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2020-11-22 21:23:22 +0100

    Added comments

--- day25.rs
+++ day25.rs
@@ -1,3 +1,12 @@
+/*
+Santa's ship is a Reindeer-class starship; these ships use pressure-sensitive floors to 
+determine the identity of droids and crew members. The standard configuration for these 
+starships is for all droids to weigh exactly the same amount to make them easier to detect. 
+If you need to get past such a sensor, you might be able to reach the correct weight by 
+carrying items from the environment.
+
+Look around the ship and see if you can find the password for the main airlock.
+*/
 fn main() {
   println!("hello world advent of code day 25");
 }
