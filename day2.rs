c0719cb36ac44e45c24585619ab99d7d02529a83
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2020-11-23 19:57:30 +0100

    Adding inputs

--- day2.rs
+++ day2.rs
@@ -1,3 +1,10 @@
+/*Here are the initial and final states of a few more small programs:
+
+1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2).
+2,3,0,3,99 becomes 2,3,0,6,99 (3 * 2 = 6).
+2,4,4,5,99,0 becomes 2,4,4,5,99,9801 (99 * 99 = 9801).
+1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99.*/
+
 fn main() {
    
     let initial_state = "1,9,10,3,2,3,11,0,99,30,40,50";
