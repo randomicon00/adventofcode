cb7f0563289d3743d89d33499f8a243fc658fb26
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2020-11-24 19:56:55 +0100

    Add problem description

--- day7.rs
+++ day7.rs
@@ -1,3 +1,21 @@
+/*
+Here are some example programs:
+
+Max thruster signal 43210 (from phase setting sequence 4,3,2,1,0):
+
+3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0
+Max thruster signal 54321 (from phase setting sequence 0,1,2,3,4):
+
+3,23,3,24,1002,24,10,24,1002,23,-1,23,
+101,5,23,23,1,24,23,23,4,23,99,0,0
+Max thruster signal 65210 (from phase setting sequence 1,0,4,3,2):
+
+3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,
+1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0
+Try every combination of phase settings on the amplifiers. What is the highest signal that can be sent to the thrusters?
+
+*/
+
 fn main() {
-  println!("hello day 7 of advent of code")
+  println!("hello day 7 of advent of code");
 }
