78708197d33656a6402333058b33412e024d17e1
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2020-11-26 23:26:55 +0100

    Add problem description

--- day9.rs
+++ day9.rs
@@ -1,3 +1,18 @@
+/*
+Your Intcode computer will also need a few other capabilities:
+
+The computer's available memory should be much larger than the initial program. Memory beyond the initial program starts with the value 0 and can be read or written like any other memory. (It is invalid to try to access memory at a negative address, though.)
+The computer should have support for large numbers. Some instructions near the beginning of the BOOST program will verify this capability.
+Here are some example programs that use these features:
+
+109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99 takes no input and produces a copy of itself as output.
+1102,34915192,34915192,7,4,7,99,0 should output a 16-digit number.
+104,1125899906842624,99 should output the large number in the middle.
+The BOOST program will ask for a single input; run it in test mode by providing it the value 1. It will perform a series of checks on each opcode, output any opcodes (and the associated parameter modes) that seem to be functioning incorrectly, and finally output a BOOST keycode.
+
+Once your Intcode computer is fully functional, the BOOST program should report no malfunctioning opcodes when run in test mode; it should only output a single value, the BOOST keycode. What BOOST keycode does it produce?
+*/
+
 fn main() {
   println!("Day9 of advent of code");
 }
