00d4a3c007468e7312c241409e8206bf2b0f6a68
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2020-12-18 19:33:30 +0100

    Adding a new test.

--- day6.rs
+++ day6.rs
@@ -2,3 +2,13 @@ fn main() {
   let hello_world = "hello world".to_string();
   println!("{}", hello_world);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_true() {
+        assert_eq!(2, 2);
+    }
+}
