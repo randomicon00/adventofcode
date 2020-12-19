24450c77fd971b7933a6a6817898666043a34c4e
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2020-12-19 12:23:51 +0100

    test modules for advent of code day 24

--- day24.rs
+++ day24.rs
@@ -4,3 +4,13 @@ What is the biodiversity rating for the first layout that appears twice?
 fn main() {
   println!("Hello world day 24 of the advent of code");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_true() {
+        assert!(true);
+    }
+}
