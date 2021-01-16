c6f7615916511cf815b5630c690b4feae817be52
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2021-01-16 19:23:20 +0100

    Advent of code day 12 input added

--- day12.rs
+++ day12.rs
@@ -6,7 +6,13 @@
 */
 
 fn main() {
-  println!("advent of code day 11");
+  
+  let input = "<x=16, y=-11, z=2>
+<x=0, y=-4, z=7>
+<x=6, y=4, z=-10>
+<x=-3, y=-2, z=-4>"
+  
+  println!("Advent of code day 11 input is: {}", input);
 }
 
 #[cfg(test)]
@@ -19,6 +25,6 @@ mod tests {
   }
   #[test]
   fn test_equal() {
-    assert_eq!(1, 1);
+    assert_eq!(true, true);
   }
 }
