73bde905fdb589f38533191b73f1ed71b165d2be
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2021-03-04 08:34:25 +0100

    Cleaning up tests

--- day15.rs
+++ day15.rs
@@ -9,9 +9,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {    
-    //access to input variables one level up the tests module.
-    use super::*;
-    
+        
     #[test]
     fn test_true() {
         assert!(true);
