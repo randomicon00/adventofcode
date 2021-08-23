ce4fbfc7112c5bf52b10b7f90e38acad8a885084
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2021-08-23 11:03:25 +0100

    Comments instead of documentation

--- day1.rs
+++ day1.rs
@@ -1,11 +1,11 @@
-/// Advent of code problem's 1 solution
-/// Problem's scenario:
-/// For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
-/// For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
-/// For a mass of 1969, the fuel required is 654.
-/// For a mass of 100756, the fuel required is 33583.
-/// Valid & tested solution
-/// Compute the quantity of fuel required.
+// Advent of code problem's 1 solution
+// Problem's scenario:
+// For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
+// For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
+// For a mass of 1969, the fuel required is 654.
+// For a mass of 100756, the fuel required is 33583.
+// Valid & tested solution
+// Compute the quantity of fuel required.
 fn fuel_required(mass: usize) -> usize {
   (mass/3) - 2
 }
