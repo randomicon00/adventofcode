96bfa8fda3d4c7b2c0c5e54f9ae378a6a14c80e3
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2022-01-24 20:26:40 +0100

    Added comment about the rest of solutions

--- day1.rs
+++ day1.rs
@@ -1,4 +1,5 @@
 /// Advent of code problem's 1 solution.
+/// Rest of solutions will follow.
 // Problem's scenario:
 // For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
 // For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
